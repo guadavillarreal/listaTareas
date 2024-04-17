cbbe566442b1e485457ba4f52fc72dcf599d5c33
guadavillarreal <guada.villarreal.gnv@gmail.com>
2024-04-16 21:48:43 -0300

    agregamos el maquetado del componente Footer y lo agregamos al padre App.jsx para que sea hermano de listarea-container

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
-import FormularioTareas from './components/FormularioTareas';
-import { Container } from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import FormularioTareas from "./components/FormularioTareas";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
-    <Container className='my-4'>
-      <h1 className='text-center display-3'>Lista de Tareas</h1>
-      <FormularioTareas/>
-    </Container>
-  )
+    <>
+      <Container className="my-4">
+        <h1 className="text-center display-3">Lista de Tareas</h1>
+        <FormularioTareas />
+      </Container>
+      <Footer></Footer>
+      {/*agregarle estilo lo que sea necesario para bajar el footer  */}
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="bg-dark text-light py-3 text-center">
+            <p>&copy; Todos los dererchos reservados</p>
+        </footer>
+    );
+};
+
+export default Footer;
